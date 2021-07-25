0ed160176096a8dded0946a3b6afd8a78952a3c3
gr2nsky <dbswovlf2009@naver.com>
2021-07-25 12:51:32 +0900

    프로젝트 생성 및 로고 이미지뷰 생성

---
+++ FirstScreen/FirstScreen/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FirstScreen
+//
+//  Created by 윤재필 on 2021/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var initialImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
